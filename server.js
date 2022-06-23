645032dfe1d01841d5ecec7c4fb8c2d4e74a60f2
Triss Marsh <marshtriss@gmail.com>
2022-06-23 01:25:58 +0100

    added db

--- server.js
+++ server.js
@@ -3,3 +3,16 @@ const app = express()
 const cors = require('cors')
 const MongoClient = require('mongodb').MongoClient
 require('dotenv').config()
+
+let db,
+    // super secret connection-string
+    dbConnectionString = process.env.DB_STRING,
+    dbName = 'template',
+    collection
+
+MongoClient.connect(dbConnectionString)
+    .then(client => {
+        console.log(`Connected to Database`)
+        db = client.db(dbName)
+        collection = db.collection('test')
+    })
